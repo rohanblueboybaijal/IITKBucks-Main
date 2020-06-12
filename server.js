04def5779e4100ea295d54497958ba0863a5e42b
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2020-06-12 17:53:02 +0530

    Minor changes with respect to representation of pendingTransactions

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const Input = require('./transaction/input');
+const Output = require('./transaction/output');
+const Transaction = require('./transaction/transaction');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,12 +15,24 @@ app.get('/getBlock/:blockIndex', (req, res) => {
     var index = req.params.blockIndex;
     var path = 'blocks/block' + index + '.dat';
     var data = fs.readFileSync(path);
+    res.set('Content-Type', 'application/octet-stream');
     res.send(data);
 });
 
 app.get('/getPendingTransactions', (req, res) => {
-    var pendingTransaction = fs.readFileSync('transactions/pendingTransactions.json', 'utf8');
-    res.send(pendingTransaction);
+    const jsonString = fs.readFileSync('./pendingTransactions.json', 'utf8');
+    var pendingTransactions = [];
+    if(jsonString) {
+        var tempArray = JSON.parse(jsonString);
+        for(let object of tempArray) {
+            var temp = [];
+            temp["inputs"] = object.inputs;
+            temp["outputs"] = object.outputs;
+            pendingTransactions.push(temp);
+        }
+    }
+    res.set('Content-Type', 'application/json');
+    res.send(pendingTransactions);
 });
 
 app.listen(8000, () => {
