fac172b8db15f096d9d4f5b2862c9cfc4df3b2e8
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2020-07-16 12:24:37 +0530

    id -> transactionId

--- transaction/input.js
+++ transaction/input.js
@@ -1,6 +1,6 @@
 class Input {
-    constructor({ id, index, signatureLength, signature }) {
-        this.id = id; //Transaction ID where the Output lies
+    constructor({ transactionId, index, signatureLength, signature }) {
+        this.transactionId = transactionId; //Transaction ID where the Output lies
         this.index = index; //Index of the  Output in the Transaction
         this.signatureLength = signatureLength;
         this.signature = signature;
